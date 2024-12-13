fe079e6e58297ad3b27aab84527c667209777429
wolf-yuan <wolf@wolf-yuan.dev>
2024-12-13 14:39:09 +0800

    chore: use turborepo for better building performance

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Presentations
 
-Some presentations
+This repository contains the slides for the presentations I've given. The slides are written in Markdown and rendered using [slidev](https://sli.dev)
---
+++ merge-slides.sh
@@ -0,0 +1,16 @@
+trap 'exit 130' INT
+
+echo "Copying all slidev presentations"
+rm -r dist/
+mkdir dist/
+
+for item in $(find ./presentations -mindepth 1 -maxdepth 1 -type d); do
+  cd "$item"
+  directory_name=${PWD##*/}
+  echo "Copying $directory_name"
+
+  mv dist/ ../../dist/$directory_name
+  cd ../../
+done
+
+echo "All presentations copied successfully"
